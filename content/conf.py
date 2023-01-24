a3c7eb3749eabdf96d8fbfcd5c8b53525a5ff82e
Richard Darst <rkd@zgib.net>
2023-01-24 12:36:56 +0200

    Add sphinx-rtd-theme

--- content/conf.py
+++ content/conf.py
@@ -25,5 +25,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_context = {'display_github': True,
+                'github_user': 'rkdarst',
+                'github_repo': 'sphinx-site-test',
+                'github_version': 'main/',
+               }
